4d098f3e80fd52033554b6b2e1cd7aaa6cfe85ba
Azoka23 <marcela68.ma@gmail.com>
2024-06-15 17:51:15 -0300

    metodos en views y models

--- app/views.py
+++ app/views.py
@@ -1,4 +1,55 @@
-from flask import jsonify
+from app.models import Usuario
+from flask import jsonify,request
+
+
 def index():
     response={}
     return jsonify(response)
+
+#Traer todas las peliculas
+def get_all_usuarios():
+    usuarios = Usuario.get_all()
+    return jsonify([usuario.serialize() for usuario in usuarios])
+
+
+#Traer un usuario
+def get_usuario(usuario_id):
+    usuario = Usuario.get_by_id(usuario_id)
+    if not usuario:
+            return jsonify({'message': 'Usuario not found'}), 404
+    return jsonify(usuario.serialize())
+
+
+#Crear un usuario
+def create_usuario():
+    data = request.json
+    new_usuario = Usuario(apellido=data['apellido'], nombre=data['nombre'],
+fecha_nacimiento=data['fecha_nacimiento'], documento=data['documento'], telefono=data['telefono'], email=data['email'], pais_origen=data['pais_origen'], password=data['password'])
+    new_usuario.save()
+    return jsonify({'message': 'Usuario created successfully'}), 201
+
+#Actualizar usuario
+def update_usuario(usuario_id):
+    usuario = Usuario.get_by_id(usuario_id)
+    if not usuario:
+        return jsonify({'message': 'Usuario not found'}), 404
+    data = request.json
+    usuario.apellido = data['apellido']
+    usuario.nombre = data['nombre']
+    usuario.fecha_nacimiento = data['fecha_nacimiento']
+    usuario.documento = data['documento']
+    usuario.telefono = data['telefono']
+    usuario.email = data['email']
+    usuario.pais_origen = data['pais_origen']
+    usuario.password = data['password']
+    get_usuario.save()
+    return jsonify({'message': 'Usuario updated successfully'})
+
+
+#Eliminar un usuario
+def delete_usuario(usuario_id):
+    usuario = Usuario.get_by_id(usuario_id)
+    if not usuario:
+          return jsonify({'message': 'Usuario not found'}), 404
+    usuario.delete()
+    return jsonify({'message': 'Usuario deleted successfully'})
--- run.py
+++ run.py
@@ -1,13 +1,19 @@
 from flask import Flask
-from app.views import index
+from app.views import index, get_all_usuarios, create_usuario, get_usuario, update_usuario, delete_usuario
 
 # Creación de la instancia de Flask
 app = Flask(__name__)
 
 # Asociación de rutas con vistas
-@app.route('/nombre-ruta', methods=['GET'])
-def nombre_de_la_funcion():
-    return index()  # Llama a la función index desde el módulo app.views
+#@app.route('/nombre-ruta', methods=['GET'])
+#def nombre_de_la_funcion():
+    #return index()  # Llama a la función index desde el módulo app.views
+app.route('/api/usuarios/', methods=['GET'])(get_all_usuarios)
+app.route('/api/usuarios/', methods=['POST'])(create_usuario)
+app.route('/api/usuarios/<int:usuario_id>', methods=['GET'])(get_usuario)
+app.route('/api/usuarios/<int:usuario_id>', methods=['PUT'])(update_usuario)
+app.route('/api/usuarios/<int:usuario_id>', methods=['DELETE'])(delete_usuario)
+
 
 # Punto de entrada para ejecutar la aplicación
 if __name__ == '__main__':
